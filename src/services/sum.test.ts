a4f837d5242678897b1194a762aa7da1e6aed02b
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2025-04-18 14:30:38 +0100

    feat - add test for sum

--- src/services/sum.test.ts
+++ src/services/sum.test.ts
@@ -3,5 +3,8 @@ import { Sum } from './sum'
 
 test('adds 1 + 2 to equal 3', () => {
     expect(Sum(1, 2)).toBe(3)
-  })
+ })
 
+ test('adds 1 + 3 to equal 4', () => {
+    expect(Sum(1, 3)).toBe(4)
+ })
